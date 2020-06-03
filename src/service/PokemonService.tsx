2727c4426ec34e1e7952e1a7444d85f97f0b9a1a
dlop4life <dlop4life@gmail.com>
2020-06-03 13:02:21 -0400

    Added params to axios requests. Added fetchPokemonByName and festPokemonList

--- src/service/PokemonService.tsx
+++ src/service/PokemonService.tsx
@@ -8,7 +8,39 @@ export class PokemonService {
     fetchPokemonByID = async (id: number) => {
         let result;
         try {
-            result = await axios.get(baseUrl+'/fetchById');
+            result = await await axios.get(baseUrl+'/fetchById', {
+                params: {
+                    id
+                }
+            });
+        } catch (error) {
+            console.log("Fetch By ID Error: " + error)
+        }
+        return result;
+    }
+
+    fetchPokemonByName = async (name: string) => {
+        let result;
+        try {
+            result = await axios.get(baseUrl+'/fetchByName', {
+                params: {
+                    name
+                }
+            });
+        } catch (error) {
+            console.log("Fetch By ID Error: " + error)
+        }
+        return result;
+    }
+
+    fetchPokemonList = async (id: number) => {
+        let result;
+        try {
+            result = await axios.get(baseUrl+'/fetchPokemonList', {
+                params: {
+                    id
+                }
+            });
         } catch (error) {
             console.log("Fetch By ID Error: " + error)
         }
